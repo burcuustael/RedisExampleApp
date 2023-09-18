efd82f7dcbe573f565020151b058561268af37c2
burcuustael <burcustael@gmail.com>
2023-09-18 22:09:56 +0300

    Cache Class Library ve Redis Service oluşturma

---
+++ RedisExampleApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repositories;
+
+namespace RedisExampleApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(await _productRepository.GetAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            return Ok(await _productRepository.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Product product)
+        {
+            return Created(string.Empty, await _productRepository.CreateAsync(product));    
+        }
+    }
+}
--- RedisExampleApp.API/Program.cs
+++ RedisExampleApp.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repositories;
+using RedisExampleApp.Cache;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +11,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductRepository , ProductRepository>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("myDatabase");
+});
+
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    return new RedisService(builder.Configuration["CacheOptions:Url"]);
+});
 
 var app = builder.Build();
+
+// uygulama ayaða kalktýktan sonra veritabanýna eriþmke için gerekli!!
+
+using (var scope= app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
